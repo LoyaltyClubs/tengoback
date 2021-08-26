5f20bd4e955b580b35fe914377101ca9e45822db
Daniel Gorianz <aalbaafikaa@gmail.com>
2021-08-26 13:28:20 -0400

    adicion de cors

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
+const cors = require("cors");
 
 app.set('port', process.env.PORT || 8000);
 
 app.use(morgan('dev'));
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
+app.use(cors());
 
 app.use("/api/pos", require("./routes/pos.routes"))
 app.use("/api/ciudad", require("./routes/ciudad.routes"))
