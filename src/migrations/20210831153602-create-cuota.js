a0d2f50a3096ea2c28aaf01585974b696e2d5ce3
Daniel Gorianz <aalbaafikaa@gmail.com>
2021-09-02 09:54:38 -0400

    defautl value deleted

--- src/migrations/20210831153602-create-cuota.js
+++ src/migrations/20210831153602-create-cuota.js
@@ -24,7 +24,8 @@ module.exports = {
         type: Sequelize.STRING
       },
       deleted: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
       },
       createdAt: {
         allowNull: false,
