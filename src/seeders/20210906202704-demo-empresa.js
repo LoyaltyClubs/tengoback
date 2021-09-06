280fd316ffba52ad0818c4ed4accd76aba3bfa6c
Daniel Gorianz <aalbaafikaa@gmail.com>
2021-09-06 17:03:34 -0400

    cambio seed empresa

--- src/seeders/20210906202704-demo-empresa.js
+++ src/seeders/20210906202704-demo-empresa.js
@@ -1,5 +1,4 @@
 'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Empresa', [{
@@ -7,10 +6,10 @@ module.exports = {
       razon_social: "Loyalty Clubs SA",
       nit: "12345678",
       rubro_id: 5,
-      direccion: "Edificio Madero Oficina #6",
+      direccion: "Edificio Madero Oficina 6",
       ciudad_id: 5,
       fecha_cierre: 5,
-      inicio_contrato: new Date(2021,01,01),
+      inicio_contrato: new Date(2021,1,1),
       fin_contrato: new Date(2021,12,31),
       representante_legal: "Mariel Rivas",
       email: "mrivas@loyaltyclubs.net",
@@ -18,7 +17,7 @@ module.exports = {
       ci: "12345678",
       expedicion: "SC",
       telefono: "7777777",
-      plan_id: 3,
+      //plan_id: 3,
       createdAt: new Date(),
       updatedAt: new Date()
     },
