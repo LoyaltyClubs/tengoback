a98ec1da7c2b397d110cc1109f28275427e18c08
Ismael Aliaga <ialiagakulat@gmail.com>
2021-09-06 15:01:18 -0400

    agregando la logica para cuotas de financiamiento

--- src/controllers/pos.controller.js
+++ src/controllers/pos.controller.js
@@ -9,25 +9,25 @@ posController.leerTarjeta = async (req, res) => {
     let datos;
     try {
         datos = dato.split('ñ');
-    }catch (error){
-        esValida="NO";
+    } catch (error) {
+        esValida = "NO";
     }
-    var isNominada = datos[0].includes("TARJETA")?false:true;
-        const resp = {
-            "element": 
-                {
-                    "esValida": esValida,
-                    "tipoTarjeta":isNominada?"Nominada":"Innominada",
-                    "cedula":isNominada?datos[1].substring(9,18):null,
-                    "version":isNominada?datos[1].substring(datos[1].length-4,datos[1].length-1):null,
-                    "nrotarjeta":datos[2].substring(0,datos[2].length-1),
-                    "mesanioexpiracion":datos[1].substring(datos[1].length-8,datos[1].length-4)
-                },
-            "errors":[],
-            "messages":[],
-            "hasErrors":false,
-            "hasMessages":false,
-        }
+    var isNominada = datos[0].includes("TARJETA") ? false : true;
+    const resp = {
+        "element":
+        {
+            "esValida": esValida,
+            "tipoTarjeta": isNominada ? "Nominada" : "Innominada",
+            "cedula": isNominada ? datos[1].substring(9, 18) : null,
+            "version": isNominada ? datos[1].substring(datos[1].length - 4, datos[1].length - 1) : null,
+            "nrotarjeta": datos[2].substring(0, datos[2].length - 1),
+            "mesanioexpiracion": datos[1].substring(datos[1].length - 8, datos[1].length - 4)
+        },
+        "errors": [],
+        "messages": [],
+        "hasErrors": false,
+        "hasMessages": false,
+    }
     res.json(resp);
 }
 
@@ -182,14 +182,10 @@ posController.finaciamiento = async (req, res) => {
         producto_credito,
         monto_financiar,
         cantidad_cuotas,
-<<<<<<< HEAD
         monto_total,
         pie,
         diferido,
         descuento
-=======
-        mensaje_codigo
->>>>>>> f690685a9cf23182265d5aaad238ffe9e17e8d0e
     } = req.body
     // var total = parseFloat(req.body.monto_financiar) + req.body.monto_financiar * 0.02;
     // var cuota = total / req.body.cantidad_cuotas;
@@ -341,8 +337,8 @@ posController.validarTarjeta = async (req, res) => {
         })
         if (resp !== null) {
             //if (resp.estado == 'vigente') {
-                respTarjetaValida(true, resp.fecha_vencimiento)
-                res.status(200).json({ tarjetaValida })
+            respTarjetaValida(true, resp.fecha_vencimiento)
+            res.status(200).json({ tarjetaValida })
             //}
         }
         else {
@@ -379,36 +375,36 @@ posController.actualizarTarjeta = (req, res) => {
 posController.pagoCuota = (req, res) => {
     //tipo_pago = "ABO", forma_pago="1", nro_comprobante="3523523"
     const resp = {
-        "comercio":req.body.comercio,//se recibe
-        "local":req.body.local,//se recibe
-        "caja":req.body.caja,//se recibe
-        "transaccion_nro":req.body.transaccion_nro,
-        "transaccion_fecha":req.body.transaccion_fecha,
-        "transaccion_hora":req.body.transaccion_hora,
-        "vendedor_nro":req.body.vendedor_nro,//se recibe
-        "transaccion_tipo":req.body.transaccion_tipo,//se recibe
-        "mensaje_codigo":req.body.mensaje_codigo,
-        "respuesta_codigo":"0000",
-        "mensaje":"Pago realizado correctamente",
-        "autorizacion_codigo":"000000000012",
-        "monto_afecto_pagado":req.body.monto_abonado
+        "comercio": req.body.comercio,//se recibe
+        "local": req.body.local,//se recibe
+        "caja": req.body.caja,//se recibe
+        "transaccion_nro": req.body.transaccion_nro,
+        "transaccion_fecha": req.body.transaccion_fecha,
+        "transaccion_hora": req.body.transaccion_hora,
+        "vendedor_nro": req.body.vendedor_nro,//se recibe
+        "transaccion_tipo": req.body.transaccion_tipo,//se recibe
+        "mensaje_codigo": req.body.mensaje_codigo,
+        "respuesta_codigo": "0000",
+        "mensaje": "Pago realizado correctamente",
+        "autorizacion_codigo": "000000000012",
+        "monto_afecto_pagado": req.body.monto_abonado
     }
     return res.status(200).json({ "element": resp, "errors": [], "messages": [], "hasError": false, "hasMessages": false });
 }
 
 posController.confirmacionFinanciamiento = (req, res) => {
     const resp = {
-        "comercio":req.body.comercio,//se recibe
-        "local":req.body.local,//se recibe
-        "caja":req.body.caja,//se recibe,
-        "transaccion_nro":req.body.transaccion_nro,
-        "transaccion_fecha":req.body.transaccion_fecha,
-        "transaccion_hora":req.body.transaccion_hora,
-        "vendedor_nro":req.body.vendedor_nro,//se recibe
-        "transaccion_tipo":req.body.transaccion_tipo,//se recibe
-        "codigo_mensaje":req.body.mensaje_codigo,
-        "codigo_respuesta":"0000",
-        "bolet_nro":"   "
+        "comercio": req.body.comercio,//se recibe
+        "local": req.body.local,//se recibe
+        "caja": req.body.caja,//se recibe,
+        "transaccion_nro": req.body.transaccion_nro,
+        "transaccion_fecha": req.body.transaccion_fecha,
+        "transaccion_hora": req.body.transaccion_hora,
+        "vendedor_nro": req.body.vendedor_nro,//se recibe
+        "transaccion_tipo": req.body.transaccion_tipo,//se recibe
+        "codigo_mensaje": req.body.mensaje_codigo,
+        "codigo_respuesta": "0000",
+        "bolet_nro": "   "
     }
     res.json({ "element": resp, "errors": [], "messages": [], "hasError": false, "hasMessages": false })
 }
