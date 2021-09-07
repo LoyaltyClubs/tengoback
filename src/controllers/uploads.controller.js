21e7bfb3aab833af62ce51cbfebe67bf479094ba
Ismael Aliaga <ialiagakulat@gmail.com>
2021-09-07 13:23:09 -0400

    creacion de registros mediante exel terminada

--- src/controllers/uploads.controller.js
+++ src/controllers/uploads.controller.js
@@ -14,7 +14,7 @@ const subirArchivo = (archivo, extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'
             return reject(`la extension ${extension} no es permitida, ${extensionesValidas}`)
         }
         const nombreTemp = `${uuidv4()}.${extension}`;
-        const uploadPath = path.join(__dirname, '../../uploads/', carpeta, nombreTemp);
+        const uploadPath = path.join(__dirname, '../uploads/', carpeta, nombreTemp);
         archivo.mv(uploadPath, (err) => {
             if (err) {
                 reject(err);
