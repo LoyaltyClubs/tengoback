6245d008fcd1df161326bbcf8579e79dd11ab7a4
Daniel Gorianz <aalbaafikaa@gmail.com>
2021-09-17 09:00:29 -0400

    cambio de ids foraneas

--- src/controllers/obtenerCiudadExel.js
+++ src/controllers/obtenerCiudadExel.js
@@ -3,37 +3,37 @@ const obtenerCiudad = (dato) => {
     let ciudad = dato.replace(' ', '').toLowerCase()
     // console.log(ciudad, 'es el dato');
 
-    if (ciudad == 'santacruz') { return 1 }
-    if (ciudad == 'lapaz') { return 2 }
-    if (ciudad == 'cochabamba') { return 3 }
-    if (ciudad == 'tarija') { return 4 }
-    if (ciudad == 'chuquisaca') { return 5 }
-    if (ciudad == 'potosi') { return 6 }
-    if (ciudad == 'oruro') { return 7 }
-    if (ciudad == 'pando') { return 8 }
-    if (ciudad == 'beni') { return 9 }
+    if (ciudad == 'santacruz') { return 5 }
+    if (ciudad == 'lapaz') { return 15 }
+    if (ciudad == 'cochabamba') { return 25 }
+    if (ciudad == 'tarija') { return 35 }
+    if (ciudad == 'chuquisaca') { return 45 }
+    if (ciudad == 'potosi') { return 55 }
+    if (ciudad == 'oruro') { return 65 }
+    if (ciudad == 'pando') { return 75 }
+    if (ciudad == 'beni') { return 85 }
 }
 
 const obtenerRubro = (dato) => {
     let ciudad = dato.replace(' ', '').toLowerCase()
     // console.log(ciudad, 'es el dato');
-    if (ciudad == 'tecnológico') { return 1 }
-    if (ciudad == 'industrial') { return 2 }
-    if (ciudad == 'alimenticio') { return 3 }
-    if (ciudad == 'electrodomésticos') { return 4 }
-    if (ciudad == 'saludybelleza') { return 5 }
-    if (ciudad == 'supermercado') { return 6 }
-    if (ciudad == 'automotriz') { return 7 }
-    if (ciudad == 'farmacias') { return 8 }
-    if (ciudad == 'tiendaderegalo') { return 9 }
+    if (ciudad == 'tecnológico') { return 5 }
+    if (ciudad == 'industrial') { return 15 }
+    if (ciudad == 'alimenticio') { return 25 }
+    if (ciudad == 'electrodomésticos') { return 35 }
+    if (ciudad == 'saludybelleza') { return 45 }
+    if (ciudad == 'supermercado') { return 55 }
+    if (ciudad == 'automotriz') { return 65 }
+    if (ciudad == 'farmacias') { return 75 }
+    if (ciudad == 'tiendaderegalo') { return 85 }
 }
 const obtenerPlan = (dato) => {
     let ciudad = dato.replace(' ', '').toLowerCase()
     // console.log(ciudad, 'es el dato');
-    if (ciudad == 'corporativogold') { return 1 }
-    if (ciudad == 'corporativoplus') { return 2 }
-    if (ciudad == 'clienteindependiente') { return 3 }
-    if (ciudad == 'corporativofuncionario') { return 4 }
+    if (ciudad == 'corporativogold') { return 5 }
+    if (ciudad == 'corporativoplus') { return 15 }
+    if (ciudad == 'clienteindependiente') { return 25 }
+    if (ciudad == 'corporativofuncionario') { return 35 }
 }
 
 
